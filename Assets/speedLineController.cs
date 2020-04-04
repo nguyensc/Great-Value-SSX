a035c72f165991f3603342a75a1fe715bc95ce26
Xamroot <toorm@mail.gvsu.edu>
2020-04-04 15:37:32 -0700

    Speedline VFX added via new particle system in player, activated when player's velocity > 30

---
+++ Assets/speedLineController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class speedLineController : MonoBehaviour
+{
+    playerController player;
+    ParticleSystem ps;
+    void Start()
+    {
+        player = FindObjectOfType<playerController>();
+        ps = GetComponent<ParticleSystem>();
+        ps.Pause();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (player.getCurrentVelocity() > 30)
+        {
+            if (ps.isPaused)
+            {
+                ps.Play();
+            }
+        }
+        else if (ps.isPlaying)
+        {
+            ps.Pause();
+        }
+        
+    }
+}
